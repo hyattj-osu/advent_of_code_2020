23ea4f25b8335669b399324d5919954c94125ecb
hyattj-osu <47150816+hyattj-osu@users.noreply.github.com>
2020-12-10 19:16:12 -0500

    day10: initial commit

---
+++ day10/day10.py
@@ -0,0 +1,11 @@
+def main():
+
+    with open("./day10/example.txt", 'r') as infile:
+        lines = [int(line) for line in infile.readlines()]
+
+    return()
+
+
+
+if __name__ == "__main__":
+    main()
